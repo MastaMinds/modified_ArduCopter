335ce15d0cdf9fa8064c86d51f7703675e85aef1
MastaMinds <ah_mm_mad@yahoo.com>
2019-05-13 06:49:42 -0400

    Defined function ModeQUNew

--- mode_qu_new.cpp
+++ mode_qu_new.cpp
@@ -2,5 +2,22 @@
 
 #if MODE_QU_NEW_ENABLED == ENABLED
 
+/*
+ * Init and run calls for circle flight mode
+ */
+
+// circle_init - initialise circle controller flight mode
+bool Copter::ModeQUNew::init(bool ignore_checks)
+{
+    return true;
+}
+
+// circle_run - runs the circle flight mode
+// should be called at 100hz or more
+void Copter::ModeQUNew::run()
+{
+    return;
+}
+
  
 #endif
